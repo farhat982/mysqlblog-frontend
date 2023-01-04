86485308397cda4c201cc47d65845a5653c877a5
farhat982 <112558257+farhat982@users.noreply.github.com>
2023-01-04 16:17:40 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,32 +20,32 @@ const Home = () => {
 		fetchData();
 	}, [cat]);
 
-	//const posts = [
-	//  {
-	//    id: 1,
-	//    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	//    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	//    img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	//  },
-	//  {
-	//    id: 2,
-	//    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	//    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	//    img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	//  },
-	//  {
-	//    id: 3,
-	//    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	//    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	//    img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	//  },
-	//  {
-	//    id: 4,
-	//    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	//    desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	//    img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	//  },
-	//]
+	const posts1 = [
+	 {
+	   id: 1,
+	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+	   img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	 },
+	 {
+	   id: 2,
+	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+	   img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	 },
+	 {
+	   id: 3,
+	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+	   img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	 },
+	 {
+	   id: 4,
+	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+	   img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+	 },
+	]
 
 	const getText = (html) => {
 		const doc = new DOMParser().parseFromString(html, 'text/html');
@@ -55,14 +55,21 @@ const Home = () => {
 	return (
 		<div className='home'>
 			<div className='posts'>
-				{posts.map((post) => (
+				{posts1.map((post) => (
 					<div
 						className='post'
 						key={post.id}
+
 					>
+// 						<div className='img'>
+// 							<img
+// 								src={`../uploads/${post.img}`}
+// 								alt='blogimg'
+// 							/>
+// 						</div>
 						<div className='img'>
 							<img
-								src={`../uploads/${post.img}`}
+								src={post.img}
 								alt='blogimg'
 							/>
 						</div>
