2d60c86e759e34686159e1b7e1f060a430524f6b
farhat982 <112558257+farhat982@users.noreply.github.com>
2023-01-04 16:21:41 +0000

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,9 +9,7 @@ const Home = () => {
 	useEffect(() => {
 		const fetchData = async () => {
 			try {
-				const response = await axios.get(
-					`/posts${cat}`
-				);
+				const response = await axios.get(`/posts${cat}`);
 				setPosts(response.data);
 			} catch (error) {
 				console.log(error);
@@ -21,31 +19,31 @@ const Home = () => {
 	}, [cat]);
 
 	const posts1 = [
-	 {
-	   id: 1,
-	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	   img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	 },
-	 {
-	   id: 2,
-	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	   img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	 },
-	 {
-	   id: 3,
-	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	   img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	 },
-	 {
-	   id: 4,
-	   title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
-	   desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
-	   img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-	 },
-	]
+		{
+			id: 1,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/7008010/pexels-photo-7008010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+		{
+			id: 2,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/6489663/pexels-photo-6489663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+		{
+			id: 3,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/4230630/pexels-photo-4230630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+		{
+			id: 4,
+			title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+			desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!',
+			img: 'https://images.pexels.com/photos/6157049/pexels-photo-6157049.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+		},
+	];
 
 	const getText = (html) => {
 		const doc = new DOMParser().parseFromString(html, 'text/html');
@@ -59,14 +57,13 @@ const Home = () => {
 					<div
 						className='post'
 						key={post.id}
-
 					>
-// 						<div className='img'>
-// 							<img
-// 								src={`../uploads/${post.img}`}
-// 								alt='blogimg'
-// 							/>
-// 						</div>
+						{/* <div className='img'>
+							<img
+								src={`../uploads/${post.img}`}
+								alt='blogimg'
+							/>
+						</div> */}
 						<div className='img'>
 							<img
 								src={post.img}
@@ -91,3 +88,4 @@ const Home = () => {
 };
 
 export default Home;
+
